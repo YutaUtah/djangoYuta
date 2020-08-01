fe12e5720fa27ae67fc04f42935d31297de58f60
Yuta Hayashi <yuta.hayashi.soccer@gmail.com>
2020-08-02 01:47:27 +0900

    until 1:37:00

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,5 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>hello world</h1>")
+def home_view(request, *args, **kwargs):
+    return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return HttpResponse("<h1>contact</h1>")
+
+def about_view(request, *args, **kwargs):
+    my_context = {
+        "my_text": "this is about my pages",
+        "my_number": 22,
+        "my_list" : [1,2,3]
+    }
+    return render(request, "about.html", my_context)
